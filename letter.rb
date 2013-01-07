1eeebeffd71fb0fa3a68530b5f9c02a59bbad5e8
Jacques Fuentes <jpfuentes2@gmail.com>
2013-01-07 13:18:27 -0500

    Fix wondeful! typo

--- letter.rb
+++ letter.rb
@@ -2,7 +2,7 @@ require "./love"
 
 a_letter to: Augusta do
   twas(only: 16.months.ago) { The::Universe << You.to(OurFamily) }
-  life.has :been => %w(i n c r e d i b l y).zip(*"wonderul!").ever_since
+  life.has :been => %w(i n c r e d i b l y).zip(*"wonderful!").ever_since
   We::Wish.we_could { experience these_moments: over & over }
   You.will always_be: Loved, and: Cherished
   until Infinity.ends do; Forever.(); end
